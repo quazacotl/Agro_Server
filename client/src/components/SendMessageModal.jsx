fbc39c3bc8bbe7508d76b1226f26f6ec8dfe9229
Vagrant <quazacotl@gmail.com>
2022-06-16 10:12:06 +0300

    Fixed tare send message text.

--- client/src/components/SendMessageModal.jsx
+++ client/src/components/SendMessageModal.jsx
@@ -33,25 +33,25 @@ const SendMessageModal = observer(() => {
 
     const setCurrentMessageText = () => {
         switch (Store.currentRequest.RequestType) {
-            case 'Весы': formState.setMessageText(`Ремонт на весах проведёны. Заявка закрыта`)
+            case 'Весы': formState.setMessageText(`Ремонт на весах проведёны. Заявка закрыта.`)
                 break
-            case 'Установка Автограф+ДУТ': formState.setMessageText(`Работы по установке оборудования на технике ${Store.currentRequest.VehicleType} ${Store.currentRequest.VehicleRegNum} проведены. Заявка закрыта`)
+            case 'Установка Автограф+ДУТ': formState.setMessageText(`Работы по установке оборудования на технике ${Store.currentRequest.VehicleType} ${Store.currentRequest.VehicleRegNum} проведены. Заявка закрыта.`)
                 break
-            case 'Установка автографа': formState.setMessageText(`Работы по установке оборудования на технике ${Store.currentRequest.VehicleType} ${Store.currentRequest.VehicleRegNum} проведены. Заявка закрыта`)
+            case 'Установка автографа': formState.setMessageText(`Работы по установке оборудования на технике ${Store.currentRequest.VehicleType} ${Store.currentRequest.VehicleRegNum} проведены. Заявка закрыта.`)
                 break
             case 'Тахограф': formState.setMessageText(`Работы с тахографом на технике ${Store.currentRequest.VehicleType} ${Store.currentRequest.VehicleRegNum} проведены. Заявка закрыта`)
                 break
-            case 'Прочее': formState.setMessageText(`Работы проведены. Заявка закрыта`)
+            case 'Прочее': formState.setMessageText(`Работы проведены. Заявка закрыта.`)
                 break
-            case 'Установка системы учета зерна/удобрений': formState.setMessageText(`Работы по установке системы учёта на технике ${Store.currentRequest.VehicleType} ${Store.currentRequest.VehicleRegNum} проведены. Заявка закрыта`)
+            case 'Установка системы учета зерна/удобрений': formState.setMessageText(`Работы по установке системы учёта на технике ${Store.currentRequest.VehicleType} ${Store.currentRequest.VehicleRegNum} проведены. Заявка закрыта.`)
                 break
-            case 'Планшеты': formState.setMessageText(`Работы проведены. Заявка закрыта`)
+            case 'Планшеты': formState.setMessageText(`Работы проведены. Заявка закрыта.`)
                 break
-            case 'Метеостанция': formState.setMessageText(`Работы на метеостанции завершены. Заявка закрыта`)
+            case 'Метеостанция': formState.setMessageText(`Работы на метеостанции завершены. Заявка закрыта.`)
                 break
-            case 'Сигнализация': formState.setMessageText(`Работы по ремонту сигнализации проведены. Заявка закрыта`)
+            case 'Сигнализация': formState.setMessageText(`Работы по ремонту сигнализации проведены. Заявка закрыта.`)
                 break
-            default: formState.setMessageText(`Работы на технике ${Store.currentRequest.VehicleType} ${Store.currentRequest.VehicleRegNum} проведены. Заявка закрыта`)
+            default: formState.setMessageText(`Работы на технике ${Store.currentRequest.VehicleType} ${Store.currentRequest.VehicleRegNum} проведены. Заявка закрыта.`)
                 break
         }
     }
@@ -82,7 +82,7 @@ const SendMessageModal = observer(() => {
         }
 
         return {
-            subject: Store.currentRequest.VehicleRegNum ? `Выполненная заявка ${Store.currentRequest.VehicleRegNum}` : 'Выполненная заявка',
+            subject: Store.currentRequest.VehicleRegNum ? `Выполненная заявка ${Store.currentRequest.VehicleRegNum}.` : 'Выполненная заявка.',
             text: formState.messageText,
             files,
             recipient: Store.currentRequest.SentFromEmail
@@ -136,8 +136,8 @@ const SendMessageModal = observer(() => {
         <>
             <h2 className={'text-lg text-sky-600 ml-3'}>Адресат: {Store.currentRequest.SentFromName ? Store.currentRequest.SentFromName : <span className={'text-red-600'}>Не выбран адресат!</span>}</h2>
             <div className={'flex gap-4 mt-2'}>
-                <button className={'px-2 py-1 rounded-lg bg-orange-400 text-white'} onClick={() => formState.editMessageText('Необходимо синхронизировать файлы в афтографе и перезапустить программу')}>Текст про автограф</button>
-                <button className={'px-2 py-1 rounded-lg bg-orange-400 text-white'} onClick={() => formState.editMessageText('Контрольный слив-залив показал расхождение в пределах допустимой нормы')}>Текст про тарировку</button>
+                <button className={'px-2 py-1 rounded-lg bg-orange-400 text-white'} onClick={() => formState.editMessageText(` Необходимо синхронизировать файлы в афтографе и перезапустить программу.`)}>Текст про автограф</button>
+                <button className={'px-2 py-1 rounded-lg bg-orange-400 text-white'} onClick={() => formState.editMessageText(' Контрольный слив-залив показал расхождение в пределах допустимой нормы.')}>Текст про тарировку</button>
             </div>
             <textarea
                 placeholder={'Текст сообщения'}
